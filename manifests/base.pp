ac59a1e59799bd4c9911610b896668592159511e
Kentaro Kuribayashi <kentarok@gmail.com>
2013-01-26 16:20:16 +0900

    Added puppet and puppet-server via epel.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,11 +1,22 @@
-user { 'kentaro':
-  shell      => '/bin/zsh',
-  password   => '$1$EdHH7egO$Q9ieV8QoLADBt.EYKrg0T1',
-  home       => '/home/kentaro',
-  managehome => true,
-  require    => Package['zsh'],
+exec { 'epel':
+  command => 'rpm -Uvh http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/i386/epel-release-6-8.noarch.rpm',
+  path    => '/bin',
+  unless  => 'rpm -qa | grep epel-release',
 }
 
-package { 'zsh':
-  ensure => 'present',
+exec { 'puppet':
+  require => Exec['epel'],
+  command => 'yum -y downgrade puppet --enablerepo=epel puppet',
+  path    => '/usr/bin',
+}
+
+exec { 'puppet-server':
+  require => Exec['epel'],
+  command => 'yum -y install puppet-server --enablerepo=epel',
+  path    => '/usr/bin',
+}
+
+service { 'puppetmaster':
+  require => Exec['puppet-server'],
+  ensure  => running,
 }
