e69d8014ff303491f67cea283b9ca2610b3fa353
Kentaro Kuribayashi <kentarok@gmail.com>
2013-02-04 19:54:29 +0900

    puppetmaster

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,5 @@
+package { 'git': }
+
 exec { 'epel':
   command => 'rpm -Uvh http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/i386/epel-release-6-8.noarch.rpm',
   path    => '/bin',
@@ -10,6 +12,18 @@ exec { 'puppet':
   path    => '/usr/bin',
 }
 
+file    { '/var/lib/puppet/modules':
+  ensure => directory,
+}
+
+exec { "install puppetlabs-vcsrepo":
+  require => [Package["git"], File["/var/lib/puppet/modules"]],
+  command => "git clone git://github.com/puppetlabs/puppetlabs-vcsrepo.git",
+  path    => '/usr/bin',
+  cwd     => '/var/lib/puppet/modules',
+  unless  => "test -d /var/lib/puppet/modules/puppetlabs-vcsrepo",
+}
+
 exec { 'puppet-server':
   require => Exec['epel'],
   command => 'yum -y install puppet-server --enablerepo=epel',
@@ -19,22 +33,26 @@ exec { 'puppet-server':
 file { '/etc/puppet/puppet.conf':
   require => Exec['puppet-server'],
   mode    => 0644,
-  content => '[main]
-    manifestdir = $confdir/shared/manifests
-
+  content => '
+[main]
     logdir = /var/log/puppet
     rundir = /var/run/puppet
 [agent]
     classfile = $vardir/classes.txt
     localconfig = $vardir/localconfig
+    pluginsync  = true
+[master]
+    manifestdir = $confdir/shared/manifests
+    modulepath  = $confdir/shared/modules:$vardir/modules
+    templatedir = $confdir/shared/templates
   ',
 }
 
 file { '/etc/puppet/fileserver.conf':
   require => Exec['puppet-server'],
   mode    => 0644,
-  content => '[mount_point]
-    path /etc/puppet/shared
+  content => '[dist]
+    path /etc/puppet/shared/dist
     allow *
   ',
 }
@@ -46,3 +64,6 @@ service { 'puppetmaster':
   ],
   ensure  => running,
 }
+
+# これがないと$lsbmajdistreleaseとかが使えない
+package { 'redhat-lsb': }
